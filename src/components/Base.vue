2b3bea8cd457e2b266f1d4109c0f065322bdb27b
Andrew Sand <140562305+asand2113@users.noreply.github.com>
2025-09-06 17:32:22 -0500

    Improving styling

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -24,14 +24,14 @@ const selectedTab = ref(1);
 
 <template>
     <v-container class="h-screen">
-	<v-row>
+	<v-row class="fill-height mx-auto" align="center" style="max-width: 75%">
 	    <v-col cols="2">
 		<v-sheet border rounded style="height: 80vh" class="position-relative">
 		    <p class="pa-4">NAVIGATION</p>
 		    <v-divider thickness="4" role="presentation" />
 		    <v-list mandatory>
 			<v-list-item v-for="item in items" :key="item.title" :title="item.title" 
-			@click="selectedTab = item.value" style="background-color: #c9daf8ff" :value="item.value" color="info" />
+			@click="selectedTab = item.value" style="background-color: #c9daf8ff" :value="item.value" color="info" class="text-center ma-1" rounded="shaped" />
 		    </v-list>
 		    <div style="text-align: center" class="position-absolute bottom-0 right-0 left-0">
 			<a target="_blank" href="https://github.com/asand2113/portfolio-website">2025 Andrew Sand</a>
