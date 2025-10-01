96db71b735b91f8dff3b81c4f02ad9cdb818acc2
Andrew Sand <140562305+asand2113@users.noreply.github.com>
2025-09-30 19:13:25 -0500

    Various bug fixes and implemented the Projects section

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
     import { shallowRef } from 'vue'
     import { ref } from 'vue'
+    import { mdiArrowLeft } from '@mdi/js'
+    import { mdiArrowRight } from '@mdi/js'
 
     const selected = shallowRef([1]);
     const selectedTab = ref(1);
@@ -21,7 +23,28 @@
       title: 'CREDITS',
       value: 4,
     },
-  ]
+  ];
+    const projects = [
+    {
+      img: 'Portfolio.PNG',
+      title: 'Portfolio Website',
+      subtitle: 'The website you are viewing now! A portfolio website made using Vue, Vite, and Vuetify.',
+      link: 'https://github.com/asand2113/portfolio-website',
+    },
+    {
+      img: 'YT-Controller.PNG',
+      title: 'yt-download-controller',
+      subtitle: 'A CLI tool to make using yt-dlp a breeze.',
+      link: 'https://github.com/asand2113/yt-download-controller',
+    },
+    {
+      img: 'Minesweeper.PNG',
+      title: 'Minesweeper C++',
+      subtitle: 'A simple terminal-based clone of Minesweeper made using C++.',
+      link: 'https://github.com/asand2113/minesweeper-cpp',
+    },
+  ];
+
 </script>
 
 <template>
@@ -37,7 +60,7 @@
 			style="background-color: #c9daf8ff" :value="item.value" color="info" class="text-center ma-1" rounded="shaped" />
 		    </v-list>
 		    <div style="text-align: center" class="position-absolute bottom-0 right-0 left-0">
-			<a target="_blank" href="https://github.com/asand2113/portfolio-website">2025 Andrew Sand</a>
+			<a target="_blank" href="https://github.com/asand2113/portfolio-website">© 2025 Andrew Sand</a>
 		    </div>
 		</v-sheet>
 	    </v-col>
@@ -59,14 +82,71 @@
 				    Software Engineer
 				</v-card-subtitle>
 				<v-card-text>
-				    Temp
+				    Education:<br/>
+				    Bachelor of Science in Software Engineering from Iowa State University.<br/>
+				    Minored in Music Technology.
 				</v-card-text>
 			    </v-card>
 			</v-card-text>
 		    </v-card>
 		    <v-card title="PROJECTS" v-if="selectedTab === 3" class="h-100 w-100" rounded="lg">
 			<v-card-text style="background-color: #c9daf8ff" class="pt-4 h-100 w-100 rounded-t-xl">
-			    Temp
+			    <v-data-iterator
+			      :items="projects"
+			      :items-per-page="3">
+
+			      <template v-slot:default="{ items }">
+				<v-container class="pa-2" fluid>
+				  <v-row dense>
+				    <v-col
+				      v-for="item in items"
+				      :key="item.title"
+				      cols="auto"
+				      md="4">
+				      <v-card class="pb-3" border flat>
+					<v-img :src="`images/${item.raw.img}`"></v-img>
+
+					<v-list-item :subtitle="item.raw.subtitle" class="mb-2">
+					  <template v-slot:title>
+					    <strong class="text-h6 mb-2">{{ item.raw.title }}</strong>
+					  </template>
+					</v-list-item>
+					<div class="ma-2">
+					    <v-btn :href="item.raw.link" target="_blank" block color="primary">Go to Project</v-btn>
+					</div>
+
+				      </v-card>
+				    </v-col>
+				  </v-row>
+				</v-container>
+			      </template>
+
+			      <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
+				<div class="d-flex align-center justify-center pa-4">
+				  <v-btn
+				    :disabled="page === 1"
+				    density="comfortable"
+				    :icon="mdiArrowLeft"
+				    variant="tonal"
+				    rounded
+				    @click="prevPage"
+				  ></v-btn>
+
+				  <div class="mx-2 text-caption">
+				    Page {{ page }} of {{ pageCount }}
+				  </div>
+
+				  <v-btn
+				    :disabled="page >= pageCount"
+				    density="comfortable"
+				    :icon="mdiArrowRight"
+				    variant="tonal"
+				    rounded
+				    @click="nextPage"
+				  ></v-btn>
+				</div>
+			      </template>
+			    </v-data-iterator>
 			</v-card-text>
 		    </v-card>
 		    <v-card title="CREDITS" v-if="selectedTab === 4" class="h-100 w-100" rounded="lg">
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
 // Vuetify
 import 'vuetify/styles'
@@ -11,6 +12,13 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
